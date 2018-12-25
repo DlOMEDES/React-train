28707131dcd40885b1eac1268c2b241bf7488b37
Diomedes <diomedes.lajara@gmail.com>
2018-12-25 00:40:24 -0500

    live with react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+// import react and react dom
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create component
+const App = () => {
+  return (
+    <div>
+      <h3>Live with ReactJS</h3>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// render component to the DOM
+ReactDOM.render(<App />, document.querySelector('#root'));
